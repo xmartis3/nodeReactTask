f043f10d22dd4e01f081b7c91a9bee6716474832
BBWonderis <martiska14lukas@gmail.com>
2026-01-27 18:26:15 +0100

    backend changes for docker

--- backend/server.ts
+++ backend/server.ts
@@ -1,39 +1,37 @@
 import * as fs from "fs";
+import { NodeData, Status } from "./types/NodeData";
+import { Response, Request } from "express";
 
 const express = require("express");
-const app = express();
-const PORT = 5050;
-const cors = require("cors");
-app.use(cors({ origin: "http://localhost:5173" }));
 
-type NodeData = {
-  id: string;
-  name: string;
-  status: "online" | "offline" | "maintenance";
-  cpuUsage: number;
-  memoryUsage: number;
-  timestamp: string;
-};
+const app = express();
+const PORT = Number(process.env.PORT ?? 5050);
+const cors = require("cors");
+const ORIGIN = process.env.FRONTEND_ORIGIN ?? "http://localhost:5173";
+
+app.use(cors({ origin: ORIGIN }));
 
 const data: NodeData[] = JSON.parse(
-  fs.readFileSync("./data/nodes.json", "utf-8")
+  fs.readFileSync(`${process.cwd()}/data/nodes.json`, "utf-8")
 );
 
 function changeUsage() {
   data.forEach((element) => {
-    if (element.status !== "online") {
+    if (element.status !== Status.online) {
       return;
     }
+
     element.cpuUsage = Math.round(Math.random() * 100);
     element.memoryUsage = Math.round(Math.random() * 100) / 10;
     element.timestamp = new Date().toISOString();
   });
 }
+
 setInterval(changeUsage, 1000);
-app.get("/api/nodes", (req: any, resp: any) => {
+app.get("/api/nodes", (req: Request, resp: Response) => {
   resp.send(data);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started`);
 });
---
+++ backend/types/NodeData.ts
@@ -0,0 +1,14 @@
+export enum Status {
+  "online" = "online",
+  "offline" = "offline",
+  "maintenance" = "maintenance"
+}
+
+export type NodeData = {
+  id: string;
+  name: string;
+  status: Status;
+  cpuUsage: number;
+  memoryUsage: number;
+  timestamp: string;
+};
